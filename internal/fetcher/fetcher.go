3bab68507356870c0cf54b9ab73839512684d267
Mark Mandriota <mark@192.168.1.52>
2024-07-13 15:16:27 +0200

    refactoring

--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -18,7 +18,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"mime/multipart"
 	"net/http"
 	"net/url"
@@ -28,6 +27,37 @@ import (
 
 	"github.com/mandriota/what-anime-tui/internal/config"
 )
+
+func decode(dst *Response, resp *http.Response) error {
+	if err := json.NewDecoder(resp.Body).Decode(&dst); err != nil {
+		return fmt.Errorf("error while decoding response: %v", err)
+	}
+	
+	if dst.Error != "" {
+		return fmt.Errorf("restAPI error: %s", dst.Error)
+	}
+
+	return nil
+}
+
+func writeImagePayload(payload io.Writer, r io.Reader, fpath string) (contentType string, err error) {
+	mwriter := multipart.NewWriter(payload)
+	
+	fImg, err := mwriter.CreateFormFile("image", filepath.Base(fpath))
+	if err != nil {
+		return "", fmt.Errorf("error while creating form file: %v", err)
+	}
+	
+	if _, err := io.Copy(fImg, r); err != nil {
+		return "", fmt.Errorf("error while copying: %v", err)
+	}
+	
+	if err := mwriter.Close(); err != nil {
+		return "", fmt.Errorf("error while closing multipart.Writer: %v", err)
+	}
+	
+	return mwriter.FormDataContentType(), nil
+}
 
 type Fetcher struct {
 	payload *bytes.Buffer
@@ -42,37 +72,6 @@ func New(cfg config.FetcherConfig) Fetcher {
 	}
 }
 
-func decode(dst *Response, resp *http.Response) error {
-	if err := json.NewDecoder(resp.Body).Decode(&dst); err != nil {
-		return fmt.Errorf("error while decoding response: %v", err)
-	}
-
-	if err := resp.Body.Close(); err != nil {
-		return fmt.Errorf("error while closing response: %v", err)
-	}
-
-	if dst.Error != "" {
-		return fmt.Errorf("restAPI error: %s", dst.Error)
-	}
-
-	return nil
-}
-
-func writeImagePayload(payload io.Writer, r io.Reader, fpath string) (contentType string, err error) {
-	mwriter := multipart.NewWriter(payload)
-	fImg, err := mwriter.CreateFormFile("image", filepath.Base(fpath))
-	if err != nil {
-		return "", fmt.Errorf("error while creating form file: %v", err)
-	}
-	if _, err := io.Copy(fImg, r); err != nil {
-		return "", fmt.Errorf("error while copying: %v", err)
-	}
-	if err := mwriter.Close(); err != nil {
-		return "", fmt.Errorf("error while closing multipart.Writer: %v", err)
-	}
-	return mwriter.FormDataContentType(), nil
-}
-
 func (f Fetcher) FetchByFile(dst *Response, path string) error {
 	fs, err := os.Open(path)
 	if err != nil {
@@ -81,6 +80,7 @@ func (f Fetcher) FetchByFile(dst *Response, path string) error {
 	defer fs.Close()
 
 	f.payload.Reset()
+	
 	contentType, err := writeImagePayload(f.payload, fs, path)
 	if err != nil {
 		return fmt.Errorf("error while writing payload: %v", err)
@@ -88,8 +88,9 @@ func (f Fetcher) FetchByFile(dst *Response, path string) error {
 
 	resp, err := http.Post(f.cfg.ApiUrlByFile, contentType, f.payload)
 	if err != nil {
-		log.Fatalln("error while sending post request:", err)
+		return fmt.Errorf("error while sending post request: %v", err)
 	}
+	defer resp.Body.Close()
 
 	return decode(dst, resp)
 }
@@ -99,6 +100,7 @@ func (f Fetcher) FetchByURL(dst *Response, path string) error {
 	if err != nil {
 		return fmt.Errorf("error while fetching: %v", err)
 	}
+	defer resp.Body.Close()
 
 	return decode(dst, resp)
 }
